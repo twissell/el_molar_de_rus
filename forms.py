0ae3cc22b4d5554ecd2e5dfbf947cc8238955b20
Giorgix <jackosinapsis@gmail.com>
2014-03-05 17:49:33 -0500

    forms manager added

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
+
+
+class ContactForm(Form):
+    name = TextField("Name", [validators.Required("Por favor escribe tu nombre.")])
+    email = TextField("Email", [validators.Required("Por favor escribe tu email."), validators.Email("Por favor escribe un email valido")])
+    subject = TextField("Subject", [validators.Required("Por favor escribe un asunto")])
+    message = TextAreaField("Message", [validators.Required("Por favor escribe un mensaje.")])
+    submit = SubmitField("Send")
