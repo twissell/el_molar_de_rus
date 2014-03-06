a8a5f45f39256079d487a0280508af9cddeeb800
Giorgix <jackosinapsis@gmail.com>
2014-03-05 19:01:03 -0500

    added captcha to form

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import Form, RecaptchaField
 from wtforms import TextField, TextAreaField, SubmitField, validators, ValidationError
 
 
@@ -7,4 +7,5 @@ class ContactForm(Form):
     email = TextField("Email", [validators.Required("Por favor escribe tu email."), validators.Email("Por favor escribe un email valido")])
     subject = TextField("Subject", [validators.Required("Por favor escribe un asunto")])
     message = TextAreaField("Message", [validators.Required("Por favor escribe un mensaje.")])
+    recaptcha = RecaptchaField()
     submit = SubmitField("Send")
